a1fc23de0f2e52a213273946d18488cd42f7cd36
unknown <rodrigomorales2@acm.org>
2021-03-23 16:26:08 -0400

    make fail a test to see a change

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -3,6 +3,7 @@ package soen.tutorial;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class ArithmeticOperationsTest {
 
@@ -14,7 +15,8 @@ public class ArithmeticOperationsTest {
 		ArithmeticOperations operations = new ArithmeticOperations();
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
-		assertEquals(expected, actual);
+		//assertEquals(expected, actual);
+		fail("must fail");
 
 	}
     /* @Test
