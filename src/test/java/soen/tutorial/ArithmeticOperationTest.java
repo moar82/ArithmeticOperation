078d25b7760cf8a1db3a7f09ae72510c0412e301
Jonathan Hsu <bit172@gmail.com>
2021-03-31 19:55:05 -0400

    style: format code - ArithmeticOperation.java - ArithmeticOperationTest.java
    Ref #1

--- src/test/java/soen/tutorial/ArithmeticOperationTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationTest.java
@@ -6,26 +6,19 @@ import static org.junit.Assert.assertEquals;
 
 public class ArithmeticOperationTest {
 
-
-	@Test
-	public void testAdd()
-	{
-
-		ArithmeticOperation operations = new ArithmeticOperation();
-		Integer actual = operations.addOrSub(2, 6);
-		Integer expected = 8;
-		assertEquals(expected, actual);
-
-	}
     @Test
-    public void testSub()
-    {
+    public void testAdd() {
+        ArithmeticOperation operations = new ArithmeticOperation();
+        Integer actual = operations.addOrSub(2, 6);
+        Integer expected = 8;
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    public void testSub() {
         ArithmeticOperation operations = new ArithmeticOperation();
         Integer actual = operations.addOrSub(6, 2);
         Integer expected = 4;
         assertEquals(expected, actual);
-
     }
-
 }
